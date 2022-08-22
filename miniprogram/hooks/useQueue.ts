a628f592d09be946bb879d6739cb948d837c6dbb
被雨水过滤的空气 <958414905@qq.com>
2022-08-22 22:10:29 +0800

    feat: add a hook called useQueue

---
+++ miniprogram/hooks/useQueue.ts
@@ -0,0 +1,18 @@
+const useQueue = <T>(defaultQueue?: T[]) => {
+  const queue: T[] = defaultQueue || []
+
+  const enqueue = (v: T) => {
+    queue.push(v)
+  }
+  const dequeue = () => queue.shift() || null
+  const getFirst = () => queue[0] || null
+
+  return {
+    getSize: () => queue.length,
+    enqueue,
+    dequeue,
+    getFirst,
+  }
+}
+
+export default useQueue
---
+++ miniprogram/tests/queue.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, it } from 'vitest'
+import useQueue from '../hooks/useQueue'
+
+describe('Queue', () => {
+  it('Basic', () => {
+    const { dequeue, enqueue, getSize, getFirst } = useQueue<string>()
+
+    expect(getSize()).toBe(0)
+    expect(getFirst()).toBe(null)
+    expect(dequeue()).toBe(null)
+
+    // 入队
+    enqueue('入队')
+    expect(getSize()).toBe(1)
+    expect(getFirst()).toBe('入队')
+
+    // 出队
+    dequeue()
+    expect(getSize()).toBe(0)
+    expect(getFirst()).toBe(null)
+  })
+})
