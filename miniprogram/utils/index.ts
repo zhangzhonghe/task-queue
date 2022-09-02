d65acf549f815cfbd4249985dac88752e03e9161
被雨水过滤的空气 <958414905@qq.com>
2022-09-03 06:14:31 +0800

    refactor: rename

---
+++ miniprogram/utils/index.ts
@@ -0,0 +1,5 @@
+export const isString = (value: any) => typeof value === 'string'
+export const isArray = (value: any) => Array.isArray(value)
+
+export const sleep = (ms: number) =>
+  new Promise(resolve => setTimeout(resolve, ms))
